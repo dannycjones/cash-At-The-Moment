f8c0a52efddec78e6a93a0ce5c1d3f45c81cb889
Daniel Carl Jones <daniel.carl@live.com>
2016-10-30 05:12:02 +0000

    Successfully retrieves ATMs nearby

--- node-server/index.js
+++ node-server/index.js
@@ -1,12 +1,38 @@
-var request = require('superagent')
+var async = require('async');
+var request = require('superagent');
 
-https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=52.4445354,-1.7677615&radius=5000&type=bank&key=AIzaSyA0ycJ9qugG2vHo4R2LY3W-81vu1g3Otj8
-request
-   .get('https://maps.googleapis.com/maps/api/place/nearbysearch/json')
-   .query({ location: '52.4445354,-1.7677615' })
-   .query({ radius: '5000' })
-   .query({ type: 'bank' })
-   .query({ key: 'AIzaSyA0ycJ9qugG2vHo4R2LY3W-81vu1g3Otj8' })
-   .end(function(err, res){
-     console.log(res.body)
-   });
+function getLocalPlaces(atmType, callback) {
+  request
+     .get('https://maps.googleapis.com/maps/api/place/nearbysearch/json')
+     .query({ location: '52.4445354,-1.7677615' })
+     .query({ radius: '2500' })
+     .query({ type: atmType })
+     .query({ key: 'AIzaSyA0ycJ9qugG2vHo4R2LY3W-81vu1g3Otj8' })
+     .end(function(err, res){
+       callback(null, res.body.results);
+     });
+}
+
+function filterListedATM(atm) {
+  return atm.name.match(/.*(Barclays|NatWest|Santander|HSBC|Lloyds|RBS|Halifax).*/);
+}
+
+async.parallel([
+    function(callback) {
+      getLocalPlaces('bank', callback)
+    },
+    function(callback) {
+      getLocalPlaces('atm', callback)
+    }
+],
+// optional callback
+function (err, results) {
+  atmArray = [];
+  results.forEach(function (resultsOfType) {
+    resultsOfType.forEach(function (result) {
+      atmArray.push(result);
+    });
+  });
+  atmArray = atmArray.filter(filterListedATM);
+  console.log(atmArray);
+});
